457fc55a74363d3f388dc0e7f20fe6c097531a96
Tushar Haldar <tushar.haldar@unity3d.com>
2021-06-24 14:00:18 -0700

    Changed bullet fire rate and data manager input passing to Ally spaceobject

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -8,20 +8,21 @@ public class BulletBehavior : MonoBehaviour
     public Rigidbody2D rb;
     public float speed;
     public float damage;
-    public float velX = 1f;
+    public float velX = 5000000f;
     public float velY = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        speed = 1f;
+        speed = 10f;
+        rb.velocity = new Vector2(velX, velY) * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(velX, velY) * speed;
+        
     }
 
     private void OnBecameInvisible()
